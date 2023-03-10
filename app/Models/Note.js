9fefa6cddb766762337f8a0342b2317a6f3ef4a5
Joseph Bowers <jsphbowers@gmail.com>
2023-03-10 13:58:23 -0700

    constructed templates

--- app/Models/Note.js
+++ app/Models/Note.js
@@ -3,9 +3,29 @@ import { generateId } from "../Utils/generateId.js"
 export class Note {
   constructor(data) {
     this.id = generateId()
-    this.name = data.name
+    this.title = data.title
     this.color = data.color
-    this.date = data.date ? new Date(data.date) : new Date()
+    this.created = data.created ? new Date(data.created) : new Date()
+    this.saved = data.saved || 'Not Saved'
     this.type = data.type
+  }
+
+  get navTemplate() {
+    return `
+                <div class="p-2 d-flex justify-content-between">
+                  <h5>${this.title}</h5>
+                  <h5>${this.color}</h5>
+                </div>
+                <div>
+                  <p>${this.ComputeDate}</p>
+                  <p>${this.saved}</p>
+                </div>
+    `
+  }
+
+  get ComputeDate() {
+    let date = this.created
+    return (date.getMonth() + 1) + '/' + (date.getDate) + '/' + (date.getFullYear)
+
   }
 }
